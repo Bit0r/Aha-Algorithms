8c54ab6cbd8391d87bfedea80f0a4f73749d99e2
Bit0r <nie_wang@outlook.com>
2019-05-11 21:20:17 +0800

    Add files via upload

---
+++ fact.c
@@ -0,0 +1,28 @@
+﻿#include<stdio.h>
+#define K 1024
+
+void mult(int a[],int n,int x){
+    int i;
+    for(i=0;i<n;i++)
+        a[i]*=x;
+    for(i=0;i<n;i++){
+        a[i+1]+=a[i]/10;
+        a[i]%=10;
+    }
+}
+
+int main(){
+    int a[K],i,n;
+    scanf("%d",&n);
+    a[0]=1;
+    for(i=1;i<K;i++)
+        a[i]=0;
+    for(i=1;i<=n;i++)
+        mult(a,K,i);
+    i=K-1;
+    while(a[i]==0)
+        i--;
+    for(;i>=0;i--)
+        printf("%d",a[i]);
+    return 0;
+}
