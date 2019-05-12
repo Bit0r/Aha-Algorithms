9165a735ba00211c617af622d59f8e7db7a7845b
旺 <nie_wang@outlook.com>
2019-05-12 18:07:10 +0800

    Update function.c

--- function.c
+++ function.c
@@ -83,3 +83,17 @@ int factorial(int n){
 		printf("%4.4d", a[i]);
 	return w;
 }
+
+
+//字符串反转
+char *mystrrev(char *str){
+    char temp;
+    int i,n;
+    n=strlen(str);
+    for(i=0;i<n/2;i++){
+        temp=str[i];
+        str[i]=str[n-1-i];
+        str[n-1-i]=temp;
+    }
+    return str;	
+}
